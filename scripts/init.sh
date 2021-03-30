1da52900170a1e9816b0312a47a779f7cb4dae73
Trinh Kien <kieict.94@gmail.com>
2021-03-30 11:43:45 +0700

    Fix script init.sh for error: component 'rustfmt' for target 'x86_64-unknown-linux-gnu' is unavailable for download for channel nightly

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
-
+echo "*** Uninstall and reinstall default toolchain nightly ***"
+rustup toolchain uninstall nightly
+rustup toolchain install nightly
 echo "*** Initializing WASM build environment ***"
-
 if [ -z $CI_PROJECT_NAME ] ; then
+    echo $CI_PROJECT_NAME
     rustup update nightly
     rustup update stable
 fi
-
 rustup target add wasm32-unknown-unknown --toolchain nightly
